8c41c474983c5a4a723e574abe8d0bebb48efc91
damhiya <damhiya@gmail.com>
2021-12-05 21:11:33 +0900

    no version

--- flake.nix
+++ flake.nix
@@ -9,7 +9,6 @@
       super@{ pkgs, lib, stdenv, ... }: {
         notsodeep = stdenv.mkDerivation {
           pname = "notsodeep";
-          version = lib.mkIf (self ? rev) self.rev;
           src = notsodeep-src;
           buildInputs = with pkgs; [ libnetfilter_queue libnfnetlink ];
           installPhase = ''
