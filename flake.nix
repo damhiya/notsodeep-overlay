24ae2140d25f4f53c9f459416313b59d8ab8ad44
damhiya <damhiya@gmail.com>
2021-12-05 20:48:22 +0900

    init

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "notsodeep nix overlay";
+
+  inputs.notsodeep-src.url = "github:farukuzun/notsodeep";
+  inputs.notsodeep-src.flake = false;
+
+  outputs = { self, notsodeep-src }: {
+    overlay = self:
+      { pkgs, lib, stdenv, ... }: {
+        notsodeep = stdenv.mkDerivation {
+          pname = "notsodeep";
+          src = notsodeep-src;
+          
+          buildInputs = with pkgs; [ libnetfilter_queue libnfnetlink ];
+          installPhase = ''
+            mkdir -p $out/bin
+            cp notsodeep $out/bin
+          '';
+          meta = with lib; {
+            description = "Active DPI circumvention utility for Linux";
+            homepage = "https://github.com/farukuzun/notsodeep";
+            license = licenses.asl20;
+            platforms = platforms.linux;
+          };
+        };
+      };
+  };
+}
