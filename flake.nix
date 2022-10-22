bf94085ef1f61a3d1273aaf16bd5a70b6303cd5c
damhiya <damhiya@gmail.com>
2022-10-22 16:58:04 +0900

    replace overlay with overlays.default

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
   description = "notsodeep nix overlay";
 
-  inputs.notsodeep-src.url =
-    "github:farukuzun/notsodeep/470532243bff92fd8314f3693c4748604078b484";
-  inputs.notsodeep-src.flake = false;
+  inputs = {
+    notsodeep-src.url =
+      "github:farukuzun/notsodeep/470532243bff92fd8314f3693c4748604078b484";
+    notsodeep-src.flake = false;
+  };
 
   outputs = { self, notsodeep-src }: {
-    overlay = self: super: {
-      notsodeep = super.callPackage ./notsodeep.nix {
+    overlays.default = final: prev: {
+      notsodeep = prev.callPackage ./notsodeep.nix {
         inherit notsodeep-src;
         version = "20190529";
       };
